8f8abc69a1146d28a94f79e27330bfe8b3fe9af6
kniazivna <olenkaknyaz@gmail.com>
2022-02-13 22:48:08 +0200

    axios + movies + genres + route + pagination + switcher + movieList + movieDetails +

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,14 @@ import React from 'react';
 import css from './Header.module.css';
 import {UserInfo} from "../UserInfo/UserInfo";
 import {ThemeSwitcher} from "../ThemeSwitcher/ThemeSwitcher";
+import {Link} from "react-router-dom";
 
 const Header = ({themeSwitch}) => {
 
     return (
         <div className={css.headerWrapper}>
             <ThemeSwitcher themeSwitch={themeSwitch}/>
-            <h1>Movies</h1>
+            <Link to={'/'}><h1>Movies</h1></Link>
             <UserInfo/>
         </div>
     );
